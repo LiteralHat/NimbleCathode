eed046d88a2a52fe8b31fc804a714c24d4267eb4
LiteralHat <90408956+LiteralHat@users.noreply.github.com>
2024-11-28 01:38:44 +1100

    add mobile navbar, add load in animation for homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,29 @@ module.exports = {
         bebas: ['bebas'],
         poppins: ['poppins'],
       },
+      animation: {
+        'fade-in': 'fadeIn 2s ease-out',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': {
+            opacity: '0',
+            transform: 'scale(1)',
+            filter: 'blur(5px)'
+
+          },
+          '10%': {
+            opacity: '0',
+            transform: 'scale(1.1)',
+            filter: 'blur(5px)'
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'scale(1)',
+            filter: 'blur(0px)'
+          },
+        },
+      },
     },
   },
   plugins: [],
